c781b20b743fa3a28e4e79b8e9dee5c41056935e
jirachanon <s6303051623098@email.kmutnb.ac.th>
2023-08-18 15:23:17 +0700

    Add event data

--- src/App.js
+++ src/App.js
@@ -3,20 +3,14 @@ import Calender from "./class-component";
 import { Content, Header, Footer } from "./func-component";
 import { Banner } from "./banners";
 import { Calculator } from "./calculator";
+import { EventData } from "./event-data";
 
 export default function App() {
     return (
         <>
-         <Header/>
-            <p>
-            <center >
-                <Calender/>
-            </center> 
-            </p>
-        <Content />
-        <Footer />
         <Banner/>
         <Calculator/>
+        <EventData/>
         </>
     );
 }
--- src/calculator.js
+++ src/calculator.js
@@ -1,25 +1,24 @@
 import React from 'react'
-export class Calculator extends React.Component {
-    n1 = 15
-    n2 = 5
 
-    calculate = (op) => {
-        let c = `${this.n1} ${op} ${this.n2}`
+export function Calculator() {
+
+    const n1 = 20
+    const n2 = 4 
+
+    const calculate = (op) => {
+        let c = `${n1} ${op} ${n2}`
         let r = eval(c)
         alert(`${c} = ${r}`)
     }
 
-    render() {
-        return (
+    return (
         <div style={{textAlign:'center', marginTop:20}}>
-        <span>{this.n1}</span>&nbsp;
-        <button onClick={() => this.calculate('+')}>+</button>&nbsp;
-        <button onClick={() => this.calculate('-')}>-</button>&nbsp;
-        <button onClick={this.calculate.bind(this, '*')}>*</button>&nbsp;
-        <button onClick={this.calculate.bind(this, '/')}>/</button>&nbsp;
-        <span>{this.n2}</span>
+        <span>{n1}</span>&nbsp;
+        <button onClick={() => calculate('+')}>+</button>&nbsp;
+        <button onClick={() => calculate('-')}>-</button>&nbsp;
+        <button onClick={calculate.bind(this, '*')}>*</button>&nbsp;
+        <button onClick={calculate.bind(this, '/')}>/</button>&nbsp;
+        <span>{n2}</span>
         </div>
-        )
-    }
-        
+    )
 }
