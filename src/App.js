6fb749818a9037e76c0a7cef1f53ae4da70b513b
jirachanon <s6303051623098@email.kmutnb.ac.th>
2023-08-18 14:59:34 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from "react";
 import Calender from "./class-component";
 import { Content, Header, Footer } from "./func-component";
+import { Banner } from "./banners";
 import { Calculator } from "./calculator";
 
 export default function App() {
-  return (
-    <>
-      <Header />
-      <p>
-        <center>
-          <Calender />
-        </center>
-      </p>
-      <Content />
-      <Footer />
-    </>
-  );
-}
+    return (
+        <>
+         <Header/>
+            <p>
+            <center >
+                <Calender/>
+            </center> 
+            </p>
+        <Content />
+        <Footer />
+        <Banner/>
+        <Calculator/>
+        </>
     );
 }
