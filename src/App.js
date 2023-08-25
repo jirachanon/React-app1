a745806bcb908a61c5f848eb5f7556fc6594a165
jirachanon <s6303051623098@email.kmutnb.ac.th>
2023-08-25 15:22:13 +0700

    25/8/2023
    -Edit dependencies
    -Add refs-function
    -Add refs-array
    -Add state-function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
-import Calender from "./class-component";
-import { Content, Header, Footer } from "./func-component";
-import { Banner } from "./banners";
-import { Calculator } from "./calculator";
-import { EventData } from "./event-data";
+import RefsFunc from "./refs-func";
+import RefsArray from "./refs-arrays";
+import MessageBox from "./state-func";
 
 export default function App() {
     return (
         <>
-        <Banner/>
-        <Calculator/>
-        <EventData/>
+        <RefsFunc/>
+        <RefsArray/>
+        <MessageBox/>
         </>
     );
 }
