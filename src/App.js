440405e1e11a1debfb411faadc63c25a2e717de2
jirachanon <s6303051623098@email.kmutnb.ac.th>
2023-09-29 15:46:59 +0700

    add Text and Checkbox

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Router3 from "./router3";
+import FormText from "./form-text";
+import FormCheck from "./form-check.";
 
 export default function App() {
     return (
-        <Router3/>
+        <FormCheck/>
     );
 }
---
+++ src/form-check..js
@@ -0,0 +1,83 @@
+import React from "react";
+
+export default function FormCheck(){
+    const switchReq = React.useRef()
+    const style = ['bold', 'italic','underline']
+    const size = ['small', 'medium', 'large', 'larger']
+    let checkedStyle = []
+    let checkedSize = ''
+
+    const onChangeCheck =(event)=>{
+        let checked = event.target.checked
+        let value = event.target.value
+        let idx = checkedStyle.indexOf(value)
+
+        if(checked && idx === -1){
+            checkedStyle.push(value)
+        }else{
+            checkedStyle.slice(idx, 1)
+        }
+    }
+
+    const onChangeRadio = (event) =>{
+        checkedSize = event.target.value
+    }
+
+    const onClickButton = () => {
+        if (switchReq.current.checked){
+            if(checkedStyle.length === 0){
+                alert('Please select style')
+                return
+            }else if(checkedSize === ''){
+                alert('Please selext size')
+                return
+            }
+        }
+
+        let msg = 'selected style(s): ' + checkedStyle.join(',')
+        msg += '\nselected size: ' + checkedSize
+        alert(msg)
+    }
+
+    return(
+        <div className="mt-4 mx-auto p-3 rounded" style={{width:'450px', background:'#cee'}}>
+            <form>
+                <span>font style</span>&nbsp;&nbsp;
+                {
+                    style.map((st, i) => {
+                        return(
+                            <div className="form-check form-check-inline mb-2">
+                                <input type="checkbox" id={'check'+i} value={st}
+                                    className="form-check-input" onChange={onChangeCheck}/>
+                                <label htmlFor={'check'+i} className="form-check-label">{st}</label>
+                            </div>
+                        )
+                    })
+                }
+                <br/>
+                <span>font size</span>&nbsp;&nbsp;
+                {
+                    size.map((sz, i) => {
+                        return(
+                            <div className="form-check form-check-inline mb-2">
+                                <input type="radio" id={'radio'+i} value={sz}
+                                    className="form-check-input" onChange={onChangeRadio}/>
+                                <label htmlFor={'radio'+i} className="form-check-label">{sz}</label>
+                            </div>
+                        )
+                    })
+                }
+                <div className="form-check form-switch mt-3">
+                    <input type="checkbox" id="sw" value="require" 
+                        className="form-check-input" ref={switchReq}/>
+                    <label htmlFor="sw" className="form-check-label">require</label>
+                </div>
+
+                <div className="text-center mt-4">
+                    <button type="button" className="btn btn-sm px-4 rounded"
+                        onClick={onClickButton}>OK</button>
+                </div>
+            </form>
+        </div>
+    )
+}
